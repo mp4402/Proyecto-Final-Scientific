314f27f3c552e76f77da0c131dd2925e71ae75b3
mp4402 <mariopisquiy@ufm.edu>
2021-07-01 10:53:57 -0600

    Update prueba.m

--- prueba.m
+++ prueba.m
@@ -4,7 +4,7 @@ excelDates = xlsread('datos.xlsx','B1:B138');
 nanElements = isnan(excelDates); %indices donde estan los valores NaN
 excelDates(nanElements) = []; %eliminar las casillas
 matlabDates = 693960 + excelDates;
-excelDates = datestr(matlabDates,2)
+excelDates = datestr(matlabDates,2);
 
 %leer horas
 excelhoras = xlsread('datos.xlsx','D1:D138');
@@ -16,29 +16,47 @@ excelhoras = datestr(excelhoras,'HH:MM');
 excelglucosa = xlsread('datos.xlsx','C1:C138');
 nanElements = isnan(excelglucosa);
 excelglucosa(nanElements) = [];
-excelglucosa
 
 %leer condicion 
-[n, condiciones] = xlsread('datos.xlsx','E3:E138')
+[n, condiciones] = xlsread('datos.xlsx','E3:E138');
 printf('\n\n\n\n\n');
-p = condiciones(136,1)
-a=condiciones{136,1}
+%p = condiciones(136,1);
+%a=condiciones{136,1};
 
-fecha_inicio = datenum(input("Ingrese fecha de inicio (dd/mm/yyyy): ",'s'),"dd/mm/yyyy");
-fecha_final = datenum(input("Ingrese fecha final (dd/mm/yyyy): ",'s'),"dd/mm/yyyy");
-
-x = 1;
-for i=1: length(excelDates)
-     if fecha_inicio >= datenum(excelDates(i),"dd/mm/yyyy") && fecha_final <= datenum(excelDates(i),"dd/mm/yyyy")
-       indices(x) = i;
-       x = x+1;
+choice = menu('Seleccione una opción','Ingreso de hora habitual en la que se toma el medicamento','Ingreso de rango de fechas','Salir');
+switch choice
+  case 1
+      hora=input("Ingrese la hora habitual: ");
+      
+case 2
+    fecha_inicio = datenum(input("Ingrese fecha de inicio (mm/dd/yy): ",'s'),"mm/dd/yy");
+    while(fecha_inicio < matlabDates(1));
+      fecha_inicio = datenum(input("Ingrese otra vez la fecha de inicio (mm/dd/yy): ",'s'),"mm/dd/yy");
+    endwhile  
+    fecha_final = datenum(input("Ingrese fecha final (mm/dd/yy): ",'s'),"mm/dd/yy");
+    while(fecha_final>matlabDates(length(matlabDates)));
+      fecha_final = datenum(input("Ingrese otra vez la fecha final (mm/dd/yy): ",'s'),"mm/dd/yy");
+    endwhile
+    
+    x = 1;
+    indices=[];
+    for i=1:length(excelDates)
+       if fecha_inicio >= matlabDates(i)
+         indices(x) = i;
+         x = x+1;
+       endif
+     endfor
+     
+     if length(indices) > 10
+        random = [indices(1): 1: indices(x)]
+        random = random(randperm (10,10))
      endif
- endfor
+  case 3
+  otherwise
+endswitch
+
  
- if length(indices) > 10
-      random = [indices(1): 1: indices(x)]
-      random = random(randperm (10,10))
- endif
+ 
  
  
  
