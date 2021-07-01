a5fe6b58a175e89b308618ba0fb36141fc23cb55
CarlosAlvarado-git <cmalvarado@ufm.edu>
2021-06-30 19:44:13 -0600

    Update prueba.m
    eliminar NaN.

--- prueba.m
+++ prueba.m
@@ -1,15 +1,26 @@
 pkg load io
-
 %leer fechas
 excelDates = xlsread('datos.xlsx','B1:B138');
+nanElements = isnan(excelDates); %indices donde estan los valores NaN
+excelDates(nanElements) = []; %eliminar las casillas
 matlabDates = 693960 + excelDates;
 for i=1: length(matlabDates)
   datestr(matlabDates(i),2)
 endfor
-
 %leer horas
-pkg load io
-excelDates = xlsread('datos.xlsx','D1:D138');
-for i=1:length(excelDates)
-datestr(excelDates(i),'HH:MM')
+excelhoras = xlsread('datos.xlsx','D1:D138');
+nanElements = isnan(excelhoras);
+excelhoras(nanElements) = [];
+for i=1:length(excelhoras)
+datestr(excelhoras(i),'HH:MM')
 endfor
+%leer mg/dlmread
+excelglucosa = xlsread('datos.xlsx','C1:C138');
+nanElements = isnan(excelglucosa);
+excelglucosa(nanElements) = [];
+excelglucosa
+%leer condicion 
+[n, condiciones] = xlsread('datos.xlsx','E3:E138')
+printf('\n\n\n\n\n');
+p = condiciones(136,1)
+printf('la condicion es '+p);
