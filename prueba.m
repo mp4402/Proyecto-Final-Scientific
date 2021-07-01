b26318556ec2ffb31d1c05a23539ded79c89459a
Javier Mazariegos <javiermazariegos@ufm.edu>
2021-07-01 09:29:57 -0600

    Update prueba.m

--- prueba.m
+++ prueba.m
@@ -17,8 +17,29 @@ excelglucosa = xlsread('datos.xlsx','C1:C138');
 nanElements = isnan(excelglucosa);
 excelglucosa(nanElements) = [];
 excelglucosa
+
 %leer condicion 
 [n, condiciones] = xlsread('datos.xlsx','E3:E138')
 printf('\n\n\n\n\n');
 p = condiciones(136,1)
 a=condiciones{136,1}
+
+fecha_inicio = datenum(input("Ingrese fecha de inicio (dd/mm/yyyy): ",'s'),"dd/mm/yyyy");
+fecha_final = datenum(input("Ingrese fecha final (dd/mm/yyyy): ",'s'),"dd/mm/yyyy");
+
+x = 1;
+for i=1: length(excelDates)
+     if fecha_inicio >= datenum(excelDates(i),"dd/mm/yyyy") && fecha_final <= datenum(excelDates(i),"dd/mm/yyyy")
+       indices(x) = i;
+       x = x+1;
+     endif
+ endfor
+ 
+ if length(indices) > 10
+      random = [indices(1): 1: indices(x)]
+      random = random(randperm (10,10))
+ endif
+ 
+ 
+ 
+ 
