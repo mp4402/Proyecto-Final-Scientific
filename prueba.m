054443951eae0e4bf35d352e521d58ffa43c1dcf
Javier Alejandro Mazariegos Godoy <61554803+Javier-Mazariegos@users.noreply.github.com>
2021-06-30 19:32:28 -0600

    Update prueba.m

--- prueba.m
+++ prueba.m
@@ -1,6 +1,15 @@
 pkg load io
+
+%leer fechas
 excelDates = xlsread('datos.xlsx','B1:B138');
 matlabDates = 693960 + excelDates;
-for i=1: length(matlabDates)-1
+for i=1: length(matlabDates)
   datestr(matlabDates(i),2)
+endfor
+
+%leer horas
+pkg load io
+excelDates = xlsread('datos.xlsx','D1:D138');
+for i=1:length(excelDates)
+datestr(excelDates(i),'HH:MM')
 endfor
